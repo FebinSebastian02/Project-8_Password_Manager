cab3a47947e1e35fe976b3b416ddbc58cf7a90c8
Febin Sebastian <febin.vadakkan@gmail.com>
2023-07-16 16:59:11 +0530

    Commiting all files together with Options.py deleted

--- Main.py
+++ Main.py
@@ -1,16 +1,57 @@
 #imports are done
-from Options import *
+from cryptography.fernet import Fernet  #pip install cryptography -> To install cryptography package
+
+# def generateKey():
+#     key = Fernet.generate_key()
+#     with open("Key.key", "wb") as keyFile:
+#         keyFile.write(key)
+def loadKey():
+    keyFile = open("Key.key", "rb")
+    #Another operations that can performed while opening a file are:-
+    #rb -> To read in bytes
+    #wb -> To write in bytes
+    key = keyFile.read()
+    keyFile.close() #Manually closing the opened file
+    return key
+
+key1 = loadKey()
+fer = Fernet(key1)  #To initialize the encryption module
+def viewPwd():
+    file = open("Passwords.txt", "r")
+    for i in file.readlines():
+        line = i.rstrip()
+    #rstrip() removes unwanted trailing characters. If there are no characters passed as arguments, it removes white spaces.
+        userName, pswd = line.split("|")
+        print("User Name:- " + userName + ", Password:- " + fer.decrypt(pswd.encode()).decode())
+def addPwd():
+     with open("Passwords.txt", "a") as file:
+        #Here, with keyword is used to automatically close a file once the operations are performed.
+        #Mainly 3 operations can be performed while opening a file,
+        #a -> append (Create a new file if existing file is not present and add a new line/ Add a new line to existing file)
+        #w -> overwrite (Overwrites the existing file with new changes)
+        #r -> read (Read an existing file)
+         userName = input("Enter a User name...")
+         pwd = input("Enter a password...")
+         file.write(userName + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+         #encode () -> To convert string to bytes
+         #decode() -> To convert byte string to regular string
 
 print("\n!!!Welcome to the Password Manager!!!")
-mas_pwd = input("\nEnter your Master Password...")
 while True:
-    choice = int(input("\nSelect an option 1) Add password 2) View password 3) Quit..."))
-    if choice == 1:
-        addPwd()
-    elif choice == 2:
-        viewPwd()
-    elif choice == 3:
-        print("\n!!!Bye, see you soon!!!")
-        quit()
+    mas_pwd =input("\nEnter your Master Password...").lower()
+    #Master password is p123
+    if mas_pwd == "p123":
+        #generateKey()
+        while True:
+            choice = int(input("\nSelect an option 1) add password 2) view password 3) quit..."))
+            if choice == 1:
+                addPwd()
+            elif choice == 2:
+                viewPwd()
+            elif choice == 3:
+                print("!!!Bye, see you soon!!! ")
+                quit()
+            else:
+                print("Invalid option entered. Please enter a valid one...")
     else:
-        print("\nInvalid option entered. Enter a valid option...")
+        print("Invalid Master Password entered. Please enter a valid one...")
--- Readme.txt
+++ Readme.txt
@@ -1,10 +1,15 @@
 PASSWORD_MANAGER
 
 Steps done:-
-1) Functions were defined and called
+1) Functions were defined and called.
 2) A while loop was used to repeat the program continuously until the user needs to exit.
-3) with keyword was used to automatically close the file once operations were performed
+3) with keyword was used to automatically close the file once operations were performed.
 4) open() used to open a file and close() used to manually close a file.
-5) if else conditions where written to execute statements based on user's choice
+5) if else conditions were written to execute statements based on user's choice.
+6) cryptography package was installed for using the Fernet module for encryption.
+7) A key is generated once with the help of Fernet module to encrypt the password.
+8) 2 files - a) Password.txt and b) Key.key are created
+9) Password when added in Password.txt file is encrypted using encrypt()
+10) Password when viewed from Password.txt file is decrypted using decrypt()
 
-Status of the project:- Incomplete
+Status of the project:- Completed
